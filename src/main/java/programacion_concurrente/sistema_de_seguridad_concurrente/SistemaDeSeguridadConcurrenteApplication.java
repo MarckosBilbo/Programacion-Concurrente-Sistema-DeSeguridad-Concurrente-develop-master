1a40e50b842d80cee9786d07aee0bdcb003ea4f5
marcos <marcgabe@myuax.com>
2024-09-28 21:06:57 +0200

    testeando diferentes ejecuciones de la app

--- src/main/java/programacion_concurrente/sistema_de_seguridad_concurrente/SistemaDeSeguridadConcurrenteApplication.java
+++ src/main/java/programacion_concurrente/sistema_de_seguridad_concurrente/SistemaDeSeguridadConcurrenteApplication.java
@@ -1,4 +1,44 @@
+// SistemaDeSeguridadConcurrenteApplication.java
 package programacion_concurrente.sistema_de_seguridad_concurrente;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import programacion_concurrente.sistema_de_seguridad_concurrente.domain.Usuario;
+import programacion_concurrente.sistema_de_seguridad_concurrente.repos.UsuarioRepository;
+
+@SpringBootApplication
+public class SistemaDeSeguridadConcurrenteApplication implements CommandLineRunner {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    public static void main(String[] args) {
+        SpringApplication.run(SistemaDeSeguridadConcurrenteApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        // Crear usuario Admin predefinido si no existe
+        if (usuarioRepository.findByNombreAndIdUsuario("admin", 1234).isEmpty()) {
+            Usuario admin = new Usuario();
+            admin.setIdUsuario(6969);
+            admin.setNombre("papito");
+            admin.setRol(Usuario.Rol.ADMIN);
+            usuarioRepository.save(admin);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+/*package programacion_concurrente.sistema_de_seguridad_concurrente;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -35,3 +75,33 @@ public class SistemaDeSeguridadConcurrenteApplication implements CommandLineRunn
         sensorMonitorService.shutdownExecutorService();
     }
 }
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+/*package programacion_concurrente.sistema_de_seguridad_concurrente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SistemaDeSeguridadConcurrenteApplication {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(SistemaDeSeguridadConcurrenteApplication.class, args);
+    }
+
+}
+
+*/
