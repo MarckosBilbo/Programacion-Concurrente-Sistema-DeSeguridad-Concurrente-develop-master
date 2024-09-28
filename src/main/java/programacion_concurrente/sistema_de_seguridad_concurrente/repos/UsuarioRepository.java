6a8fd51f79651e96046c7a252e78d5f96e766d42
marcos <marcgabe@myuax.com>
2024-09-28 21:06:02 +0200

    Lo mismo (fuera paja)
    Todo lo que pertenacia a clases neutralizadas fuera

--- src/main/java/programacion_concurrente/sistema_de_seguridad_concurrente/repos/UsuarioRepository.java
+++ src/main/java/programacion_concurrente/sistema_de_seguridad_concurrente/repos/UsuarioRepository.java
@@ -1,19 +1,9 @@
 package programacion_concurrente.sistema_de_seguridad_concurrente.repos;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import programacion_concurrente.sistema_de_seguridad_concurrente.domain.Credenciales;
-import programacion_concurrente.sistema_de_seguridad_concurrente.domain.Rol;
 import programacion_concurrente.sistema_de_seguridad_concurrente.domain.Usuario;
-
+import java.util.Optional;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
-
-    Usuario findFirstByUsuario(Credenciales credenciales);
-
-    Usuario findFirstByUsuarioo(Rol rol);
-
-    boolean existsByUsuarioNombre(Integer nombre);
-
-    boolean existsByUsuariooIdRol(Long idRol);
-
+    Optional<Usuario> findByNombreAndIdUsuario(String nombre, Integer idUsuario);
 }
