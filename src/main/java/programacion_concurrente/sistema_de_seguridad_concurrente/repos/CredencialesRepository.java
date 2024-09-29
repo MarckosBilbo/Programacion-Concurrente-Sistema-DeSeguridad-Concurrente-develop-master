5ec344abbd21e26f86530445e417c172270ecafd
marcos <marcgabe@myuax.com>
2024-09-29 22:36:17 +0200

    Limpieza definitiva
    Ultimo borrado masivo de clases que resultaron ser inecesarias/incoherentes para la adpatacion final de la entrega

--- src/main/java/programacion_concurrente/sistema_de_seguridad_concurrente/repos/CredencialesRepository.java
+++
@@ -1,9 +0,0 @@
-/*package programacion_concurrente.sistema_de_seguridad_concurrente.repos;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import programacion_concurrente.sistema_de_seguridad_concurrente.domain.Credenciales;
-
-
-public interface CredencialesRepository extends JpaRepository<Credenciales, Integer> {
-}
-*/
